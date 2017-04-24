2cba53e213d864a587278429154bf68fedffdf4a
Gary Williams <gary@lbm.co>
2017-04-24 11:32:55 -0400

    It wasn't courier.... lol

--- src/LaravelStyleGuide/LSGServiceProvider.php
+++ src/LaravelStyleGuide/LSGServiceProvider.php
@@ -30,7 +30,7 @@ class LSGServiceProvider extends ServiceProvider {
         $this->loadViewsFrom(__DIR__.'/../views', $this->packageName);
 
         $this->publishes([
-            __DIR__.'/../views' => resource_path('vendor/views/courier'),
+            __DIR__.'/../views' => resource_path('vendor/views/laravel-style-guide'),
         ]);
 
         // Register your asset's publisher
--- src/routes.php
+++ src/routes.php
@@ -7,19 +7,26 @@
    return view('laravel-style-guide::styleguide');
  });
 
-$templatesPath = resource_path('views/vendor/laravel-style-guide/sections/');
+try {
+  $templatesPath = resource_path('views/vendor/laravel-style-guide/sections/');
 
-$dir = new DirectoryIterator($templatesPath);
-foreach ($dir as $fileinfo) {
-  if (!$fileinfo->isDot()) {
-    $info = pathinfo($fileinfo);
-    Route::get('/style-guide/' . str_replace('.blade', '', $info['filename']), function () use ($info) {
-      $path = base_path('vendor/dolbex/laravel-style-guide/src/views/sections/');
-      if (!strpos($info['filename'], '.blade')) {
-        return File::get($path . '/' . $info['basename']);
-      } else {
-        return view('laravel-style-guide::sections.' . str_replace('.blade', '', $info['filename']));
-      }
-    });
+  $dir = new DirectoryIterator($templatesPath);
+  foreach ($dir as $fileinfo) {
+    if (!$fileinfo->isDot()) {
+      $info = pathinfo($fileinfo);
+      Route::get('/style-guide/' . str_replace('.blade', '', $info['filename']), function () use ($info) {
+        $path = base_path('vendor/dolbex/laravel-style-guide/src/views/sections/');
+        if (!strpos($info['filename'], '.blade')) {
+          return File::get($path . '/' . $info['basename']);
+        } else {
+          return view('laravel-style-guide::sections.' . str_replace('.blade', '', $info['filename']));
+        }
+      });
+    }
+    }
+} catch (UnexpectedValueException $e) {
+  if (!App::runningInConsole())
+  {
+    throw new Exception("You must publish laravel style guide. Please run 'php artisan vendor:publish' from the command line to continue");
   }
 }
