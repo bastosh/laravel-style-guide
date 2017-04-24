051150088074d247d11ae16abbc8bef9e51e807c
Gary Williams <gary@lbm.co>
2017-04-24 12:26:26 -0400

    Routes file loading corrected path

--- src/routes.php
+++ src/routes.php
@@ -14,16 +14,17 @@ try {
   foreach ($dir as $fileinfo) {
     if (!$fileinfo->isDot()) {
       $info = pathinfo($fileinfo);
-      Route::get('/style-guide/' . str_replace('.blade', '', $info['filename']), function () use ($info) {
-        $path = base_path('vendor/dolbex/laravel-style-guide/src/views/sections/');
-        if (!strpos($info['filename'], '.blade')) {
-          return File::get($path . '/' . $info['basename']);
-        } else {
-          return view('laravel-style-guide::sections.' . str_replace('.blade', '', $info['filename']));
+      Route::get('/style-guide/' . str_replace('.blade', '', $info['filename']),
+        function () use ($info, $templatesPath) {
+          if (!strpos($info['filename'], '.blade')) {
+            return File::get($templatesPath . $info['basename']);
+          } else {
+            return view('laravel-style-guide::sections.' . str_replace('.blade', '', $info['filename']));
+          }
         }
-      });
+      );
     }
-    }
+  }
 } catch (UnexpectedValueException $e) {
   if (!App::runningInConsole())
   {
