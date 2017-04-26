16efa85e2bef972e809a0205ddc438a9db8352a9
Gary Williams <gary@lbm.co>
2017-04-26 08:06:00 -0400

    Changing how to attach

--- src/assets/laravel-style-guide-framework/src/main.js
+++ src/assets/laravel-style-guide-framework/src/main.js
@@ -1,15 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 
-window.StyleGuideVue = Vue
-
-Vue.use(VueResource)
-
-Vue.config.productionTip = false
+window.StyleGuide = require('vue');
+window.StyleGuide.use(VueResource)
+window.StyleGuide.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
